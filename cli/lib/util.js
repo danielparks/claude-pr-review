1733b4f067259d7490ca7e6b39a7bd0fd8507de4
Daniel Parks <dp+git@oxidized.org>
2026-07-27 20:07:16 -0700

    Note size of top-level comment in `list-queue`.

--- cli/lib/util.js
+++ cli/lib/util.js
@@ -1,5 +1,9 @@
 export class CliError extends Error {}
 
+export function countLines(content) {
+  return content.replace(/\n$/, "").split(/\n/).length;
+}
+
 export function fail(message) {
   throw new CliError(message);
 }
